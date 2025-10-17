a08001cbc4ddc8a0846890a6b1057a39d89f4164
Amna-Amna <mailtoamna4@gmail.com>
2025-10-17 16:20:21 +0500

    Add URL patterns for user reviews and query reviews by username

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('watchlist/<int:pk>/reviews/', views.ReviewList.as_view(), name='review-list'),
     path('watchlist/<int:pk>/create-review/', views.ReviewCreate.as_view(), name='review-create'),
     path('watchlist/<int:pk>/review/<int:review_pk>/', views.ReviewDetail.as_view(), name='review-detail'),
+    path('user-reviews/<str:username>/', views.UserReview.as_view(), name='user-review'),
+    path('query-reviews/', views.UserReviewQueryParams.as_view(), name='user-review-query-params'),
 ]
