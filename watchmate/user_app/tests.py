92f7f443d368371d6a70afdd7dcadc501c73fe3a
Amna-Amna <mailtoamna4@gmail.com>
2025-10-23 12:53:57 +0500

    Add user registration, login, and logout tests using APITestCase

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class RegistrationTestCase(APITestCase):
+    def test_registration(self):
+        data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'testpassword',
+            'confirm_password': 'testpassword'
+        }
+
+
+class LoginLogoutTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+
+    def test_login(self):
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token= Token.objects.get(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Token.objects.count(), 0)
