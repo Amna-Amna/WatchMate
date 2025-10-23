40d595f6393d9aa459b79d4f641c989e61045dcf
Amna-Amna <mailtoamna4@gmail.com>
2025-10-23 13:22:34 +0500

    Add tests for stream platform creation with permission checks using APITestCase

--- watchmate/watchlist_app/tests.py
+++ watchmate/watchlist_app/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from watchlist_app import models
 
-# Create your tests here.
+
+class StreamPlatformTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+
+    def test_stream_platform_create_forbidden(self):
+        data = {
+            'name': 'Netflix',
+            'about': 'Netflix is a streaming service that offers a wide variety of TV shows and movies.',
+            'website': 'https://www.netflix.com'
+        }
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('stream-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_stream_platform_create_as_staff(self):
+        self.user.is_staff = True
+        self.user.save()
+        data = {
+            'name': 'Netflix',
+            'about': 'Netflix is a streaming service that offers a wide variety of TV shows and movies.',
+            'website': 'https://www.netflix.com'
+        }
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('stream-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
